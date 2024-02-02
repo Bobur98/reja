95194fd0e297dbb19b8cf63dfe99e818719e578b
Bobur98 <bmuhibullaev@mail.ru>
2024-02-02 11:09:55 +0900

    dars 17: file modules

---
+++ dars17_file_module/hisob.js
@@ -0,0 +1,19 @@
+let hisob = {};
+
+hisob.qoshish = (a, b) => {
+  return a + b;
+};
+
+hisob.ayrish = (a, b) => {
+  return a - b;
+};
+
+hisob.kopaytirish = (a, b) => {
+  return a * b;
+};
+
+hisob.bolish = (a, b) => {
+  return a / b;
+};
+
+module.exports = hisob;

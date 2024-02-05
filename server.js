3d64dc88866bc936515e7fa5e751a36127b4c958
Bobur98 <bmuhibullaev@mail.ru>
2024-02-05 12:25:26 +0900

    18-19 darslar

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("Web serverni boshlash");
 const express = require('express')
+const res = require('express/lib/response')
 const app = express();
 const http =require('http')
 
@@ -15,13 +16,15 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 4: Routing code
-app.get('/hello', function(req, res) {
-   res.end(`<h1>Hello World by Brain</h1>`)
+app.post('/create-item', (req, res)=>{
+    console.log(req.body)
+    res.json({test: 'success'})
 })
 
-app.get('/gift', function(req, res) {
-    res.end(`<h1>siz sovgalar bolimidasiz</h1>`)
- })
+app.get('/', function(req, res) {
+   res.render(`harid.ejs`)
+})
+
 
 const server = http.createServer(app);
 let PORT = 3000;
--- use_hisob.js
+++
@@ -1,6 +0,0 @@
-const calculate = require('./hisob.js');
-
-const natija1 = calculate.bolish(8,2);
-const natija2 = calculate.ayrish(8,2);
-const natija3 = calculate.kopaytirish(8,2);
-const natija4 = calculate.qoshish(8,2);

5c1a2450a085eb0ed98c641361d5828217521011
Bobur98 <bmuhibullaev@mail.ru>
2024-02-12 00:02:40 +0900

    CRUD operations

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ console.log("Web serverni boshlash");
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
-const fs = require('fs');
+// const fs = require('fs');
 
 // call mongodb
 const db = require('./server.js').db();
 
 
-let user;
+// let user;
 
-fs.readFile('database/user.json', 'utf8', (err, data)=>{
-    if(err){
-        console.log("ERROR:", err)
-    } else {
-       user = data && JSON.parse(data)
-    }
-})
+// fs.readFile('database/user.json', 'utf8', (err, data)=>{
+//     if(err){
+//         console.log("ERROR:", err)
+//     } else {
+//        user = data && JSON.parse(data)
+//     }
+// })
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -31,16 +31,31 @@ app.set('view engine', 'ejs');
 
 // 4: Routing code
 app.post('/create-item', (req, res)=>{
-    console.log(req.body)
-    res.json({test: 'success'})
+    const new_reja = req.body.reja;
+    db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end('something went wrong')
+        } else {
+            res.end('successfully added')
+        }
+    })
 })
 
 app.get('/', function(req, res) {
-   res.render(`reja.ejs`)
+    console.log('user entered /');
+    db.collection('plans').find().toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end('Someting went wrong')
+        } else {
+            res.render('reja', {item: data})
+        }
+    })
 })
 
-app.get('/author', function(req, res) {
-    res.render(`author.ejs`, {user: user})
- })
+// app.get('/author', function(req, res) {
+//     res.render(`author.ejs`, {user: user})
+//  })
  
 module.exports = app

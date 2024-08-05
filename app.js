0343fe74a1a712cac779259cac2b290cbf25bb96
Bobur98 <bmuhibullaev@mail.ru>
2024-08-05 14:54:57 +0900

    fix: modify app.js & config.js

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 console.log("Web serverni boshlash");
+
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
-// const fs = require('fs');
+const morgan = require('morgan');
+const { MORGAN_FORMAT } = require('./config.js');
+const mongodb = require('mongodb');
 
-// call mongodb
+// Call mongodb
 const db = require('./server.js').db();
-const mongodb = require('mongodb')
-
-// let user;
-
-// fs.readFile('database/user.json', 'utf8', (err, data)=>{
-//     if(err){
-//         console.log("ERROR:", err)
-//     } else {
-//        user = data && JSON.parse(data)
-//     }
-// })
 
 // 1: Kirish code
 app.use(express.static("public"));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(MORGAN_FORMAT));
 
 // 2: Sessions code
 
@@ -33,49 +26,49 @@ app.set('view engine', 'ejs');
 app.post('/create-item', (req, res) => {
     const new_reja = req.body.reja;
     db.collection('plans').insertOne({ reja: new_reja }, (err, data) => {
-        res.json(data.ops[0])
-    })
-})
+        res.json(data.ops[0]);
+    });
+});
 
 app.post('/delete-item', (req, res) => {
     const id = req.body.id;
     db.collection('plans')
-        .deleteOne({ _id: new mongodb.ObjectId(id) }, function (err, data) {
-            res.json({ state: 'success' })
-        })
-})
+        .deleteOne({ _id: new mongodb.ObjectId(id) }, (err, data) => {
+            res.json({ state: 'success' });
+        });
+});
 
 app.post('/edit-item', (req, res) => {
     const data = req.body;
     console.log(data);
     db.collection('plans')
-        .findOne({ _id: new mongodb.ObjectId(data.id) }, { $set: { reja: data.new_input } }, function (err, data) {
-            res.json({ state: 'hamma rejalar ochirildi' })
-        })
-})
+        .findOneAndUpdate({ _id: new mongodb.ObjectId(data.id) }, { $set: { reja: data.new_input } }, (err, data) => {
+            res.json({ state: 'hamma rejalar ochirildi' });
+        });
+});
 
 app.post('/delete-all', (req, res) => {
     if (req.body.delete_all) {
-        db.collection('plans').deleteMany(function () {
-            res.json({ state: 'all plans are deleted' })
-        })
+        db.collection('plans').deleteMany(() => {
+            res.json({ state: 'all plans are deleted' });
+        });
     }
-})
+});
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     console.log('user entered /');
     db.collection('plans').find().toArray((err, data) => {
         if (err) {
             console.log(err);
-            res.end('Someting went wrong')
+            res.end('Someting went wrong');
         } else {
-            res.render('reja', { items: data ? data : '' })
+            res.render('reja', { items: data ? data : '' });
         }
-    })
-})
+    });
+});
 
 // app.get('/author', function(req, res) {
-//     res.render(`author.ejs`, {user: user})
-//  })
+//     res.render(`author.ejs`, {user: user});
+// });
 
-module.exports = app
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,5 @@
+const MORGAN_FORMAT = ':method :url :response-time ms [:status]';
+
+module.exports = {
+  MORGAN_FORMAT,
+};
